ec4c6d6d5cdd0ec27332155f2511ef716d97745a
DavidStudium <davidbfciclo@gmail.com>
2017-06-05 01:40:27 +0200

    Add files via upload

---
+++ php/contacta.php
@@ -0,0 +1,22 @@
+﻿<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+$para = 'jpueesc@gmail.com';
+$asunto = 'Contacta Enchufados';
+$correo = "
+Nombre del remitente: '.$nombre'
+E-mail: '.$email'
+Telefono: '.$telefono'
+Mensaje: '.$mensaje'
+";
+  
+if ($_POST['submit']) {
+if (mail($para, $asunto,utf8_decode( $correo))) {
+echo '<script type="text/javascript">alert("Los datos han sido enviados correctamente");window.location.href="http://grupalstudium.ddns.net/html/contacta.html"</script>';
+} else {
+echo 'Falló el envio';
+}
+}
+?>
